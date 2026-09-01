33d7e81e65e39fbd246af386605651bdcc8cc556
Epa Fe <epaweb3js@gmail.com>
2026-09-01 11:03:43 +0700

    update backend

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+// 1. IMPORT FILE CONFIG DATABASE AGAR OTOMATIS TERHUBUNG SAAT SERVER START
+require('./config/db'); 
+
 const quizRouter = require('./routes/quizRouter');
 
 const app = express();
@@ -19,6 +22,7 @@ app.get('/', (req, res) => {
     res.send('Backend Node.js Berhasil Berjalan!');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server aktif di port http://localhost:${PORT}`);
+// 2. TAMBAHKAN '0.0.0.0' AGAR RAILWAY BISA MENERUSKAN TRAFIK INTERNET DENGAN AMAN
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server aktif dan mendengarkan di port: ${PORT}`);
 });
